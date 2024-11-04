d40f6d68a80b745300dd5b3d4ff589366b1bace5
rahulguptha <nanirahul2002@gmail.com>
2024-11-04 20:56:29 +0530

    Added empty project generated from Spring Initilzr

---
+++ src/main/java/com/example/RahulAssignment/RahulAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.RahulAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahulAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RahulAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RahulAssignment/RahulAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RahulAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahulAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
